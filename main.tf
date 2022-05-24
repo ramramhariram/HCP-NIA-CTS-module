73fa2a06ede3ded80433a5cf910b3c4597ca015a
ramramhariram <hari@hashicorp.com>
2022-05-24 13:56:11 -0700

    adding in aws region

--- main.tf
+++ main.tf
@@ -7,7 +7,15 @@ terraform {
 		}
 	}
 }
-
+provider "aws" {
+  region  = "us-east-1"
+  #version = "~> 3.0"
+  #shared_credentials_file = ""
+  #profile = ""
+  #access_key = "AWS_ACCESS_KEY_ID"
+  #secret_key = "AWS_SECRET_ACCESS_KEY"
+  #token = "AWS_SESSION_TOKEN"
+}
 resource "aws_security_group_rule" "group" {
 	for_each          = var.services
   type              = "ingress"
